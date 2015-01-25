0057507e1c4b12b60af86209942f1270659c435a
hpljack <258146182@qq.com>
2015-01-25 18:25:25 +0800

    add more

--- src/1_16.scm
+++ src/1_16.scm
@@ -1,5 +1,14 @@
-(define (expt b n)
-	(if (= n 0)
-		1
-		(* b (expt b (- n 1)))))
-		
+(define (fast-expt b n)
+    (expt-iter b n 1))
+
+(define (expt-iter b n a)
+    (cond ((= n 0)
+            a)
+          ((even? n)
+            (expt-iter (square b)
+                       (/ n 2)
+                       a))
+          ((odd? n)
+            (expt-iter b
+                       (- n 1)
+                       (* b a)))))
